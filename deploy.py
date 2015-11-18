f4673fcaa9e383a9d7177b9807738757e128c56c
Alan Du <alanhdu@gmail.com>
2015-11-18 18:21:10 -0500

    Add small deploy script to symlink things for me

---
+++ deploy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from __future__ import print_function
+import os
+
+mapping = {"gitconfig": "~/.gitconfig",
+           "nvim": "~/.config/nvim"}
+
+os.chdir(os.path.dirname(__file__))
+
+for src, dest in mapping.items():
+    src = os.path.abspath(src)
+    dest = os.path.expanduser(dest)
+    if not os.path.exists(dest):
+        print("Symlinking", src, "to", dest)
+        os.symlink(src, dest)
